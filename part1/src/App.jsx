2ceb47bd4a413d00e21b6219ac41ecce0158333a
EvanaSabatella1989 <eva.sabatella@gmail.com>
2024-07-22 20:26:56 -0300

    Ejercicios parte 1d ejercicio 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,22 +1,12 @@
 import { useState } from "react"
 
-const App = () => {
-  //guardar los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({ good, neutral, bad }) => {
   // Calcular las estadísticas adicionales
   const total = good + neutral + bad;
   const average = total === 0 ? 0 : (good - bad) / total;
   const positivePercentage = total === 0 ? 0 : (good / total) * 100;
-
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+  return(
+    <>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -24,6 +14,26 @@ const App = () => {
       <p>all {total}</p>
       <p>average {average}</p>
       <p>positive {positivePercentage} %</p>
+    </>
+    )
+}
+
+const App = () => {
+  //guardar los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
